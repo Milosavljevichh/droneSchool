c1ecce3295cf0c420f5e50f3dbcb1f4d7a185388
Milosavljevichh <milosmilosavljevic.tesla@gmail.com>
2025-04-26 22:13:31 +0200

    added overview and summarazation

--- src/pages/LessonDetails.jsx
+++ src/pages/LessonDetails.jsx
@@ -3,8 +3,10 @@ import { useState, useEffect, cloneElement } from "react";
 import { lessons } from "../lessons/index.js"; 
 import { Typography, Box } from "@mui/material";
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
 import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import { Link as ScrollLink } from 'react-scroll'; // For smooth scrolling
 import { border, borderRadius, color, display, fontWeight, height, margin, minHeight, padding, width } from "@mui/system";
 
 export default function LessonDetails() {
@@ -17,10 +19,11 @@ export default function LessonDetails() {
   }
 
   const [lessonBody, setLessonBody] = useState(null);
+  const [summaryBody, setSummaryBody] = useState(null);
 
   const styles = {
     page:{
-        padding:'10vh 13vw',
+        padding:'10vh 7vw 10vh 13vw',
         minHeight:'100vh',
         height:'fit-content',
         color:"#ECEFCA"
@@ -43,11 +46,17 @@ export default function LessonDetails() {
     paragraph:{
         marginBottom:"46px"
     },
+    overviewParagraph:{
+        marginBottom:"20px"
+    },
     image:{
         width:'100%',
         borderRadius:'12px',
         border:'2px solid #ECEFCA',
         marginBottom:"46px"
+    },
+    list:{
+        marginBottom:"46px",
     }
   }
 
@@ -57,27 +66,54 @@ export default function LessonDetails() {
     });
   }, [lesson.file]);
 
+  useEffect(()=>{
+    let summaryBody = []
+    if (lessonBody) {
+        lessonBody.forEach((element) => {
+            if (element.type === 'subtitle') {
+                summaryBody.push(element.content)
+            }
+        })
+    }
+    setSummaryBody(summaryBody)
+  }, [lessonBody])
+
   function createElement(type, content, alt) {
     let element
     switch(type){
         case "paragraph":
             element = <Typography variant="body1" component="p" sx={styles.paragraph}>{content}</Typography>
             break;
+        case "overviewParagraph":
+            element = <Typography variant="body1" component="p" sx={styles.overviewParagraph}>{content}</Typography>
+            break;
         case "subtitle":
-            element = <Typography variant="h4" component="h2" sx={styles.subtitle}>{content}</Typography>
+            element = <Typography variant="h4" component="h2" sx={styles.subtitle} id={content}>{content}</Typography>
             break;
         case "image":
-            element = <Box sx={{margin:'0 auto', width:'50%'}}><img src={content} alt={alt} style={styles.image} /></Box>
+            element = <Box sx={{margin:'0 auto', width:'65%'}}><img src={content} alt={alt} style={styles.image} /></Box>
+            break;
+        case "list":
+            element =
+            <List dense={false} sx={styles.list}> 
+                {content.map((item)=>(
+                    <ListItem  sx={{ display: 'list-item', listStyleType: 'circle', pl: 0, marginLeft:'32px' }}>
+                        <ListItemText primary={item} />
+                    </ListItem>
+                ))}
+            </List>
             break;
     }
     return element
   }
 
   function generate(element) {
-    return [0, 1, 2].map((value) =>
-      cloneElement(element, {
-        key: value,
-      }),
+    return element.map((subtitle, index) =>
+        <ListItemButton key={"summary"+index} component={ScrollLink} to={subtitle} smooth={true} duration={500}>
+            <ListItemText
+                primary={subtitle}
+            />
+        </ListItemButton>
     );
   }
   
@@ -94,22 +130,15 @@ export default function LessonDetails() {
                         <Typography variant="h6"  component="h5" sx={styles.lessonSubtitle}>{lesson.level} level</Typography>
                     </Box>
                 </Box>
-                <Typography variant="h4"  component="h4" sx={styles.subtitle}>Introduction</Typography>
                 {lessonBody.map((element)=>(
                     createElement(element.type, element.content, element.alt)
                 ))}
             </Box>
-            <Box sx={{position:'relative'}}>
+            <Box sx={{position:'relative', marginLeft:'6vw'}}>
                 <Box sx={{position:'sticky', top:'10vh'}}>
                 <Typography variant="h5"  component="h5" sx={{marginBottom:"26px", fontWeight:'500'}}>Lesson Content</Typography>
                 <List dense={false} sx={styles.summary}>
-                {generate(
-                    <ListItem>
-                    <ListItemText
-                        primary="Single-line item"
-                    />
-                    </ListItem>,
-                )}
+                    {generate(summaryBody)}
                 </List>
                 </Box>
             </Box>
